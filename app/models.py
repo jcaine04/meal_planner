d0e23b68bfec4131d5cfd14e001bbc7a5f3b0ba7
John Caine <john@johnjcaine.com>
2018-05-15 06:27:44 -0400

    removed flask stuff. went a little overboard and going to dumb it down for now

--- app/models.py
+++ app/models.py
@@ -1,10 +1,16 @@
-from app import db
+
+from sqlalchemy import Column, Integer, String, Boolean
 
 class Recipe(db.Model):
     __tablename__ = 'recipes'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    type = db.Column(db.String(25))
     prep_time_minutes = db.Column(db.Integer)
     cook_time_minutes = db.Column(db.Integer)
+    recipe_source = db.Column(db.String())
+    vegetable_required = db.Column(db.Boolean)
+    starch_required = db.Column(db.Boolean)
 
 class Ingredient(db.Model):
     __tablename__: 'ingredients'
