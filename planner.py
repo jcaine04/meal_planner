124140a4f738325a9fedfd65e1b6decb43ae90fe
John Caine <john@johnjcaine.com>
2018-05-15 06:30:05 -0400

    moving everything up to project root. modify models a bit

---
+++ planner.py
@@ -0,0 +1,25 @@
+
+
+class Planner:
+    def __init__(self):
+        self.meals = set()
+
+    def get_meals():
+        pass
+
+
+class Meal:
+    def __init__(self):
+        self.main_dish = None
+        self.vegetable = None
+        self.starch = None
+
+    def get_main(self):
+        pass
+
+    def get_vegetable(self):
+        pass
+
+    def get_starch(self):
+        pass
+
